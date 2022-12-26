d04d44ba7cd8f9f6a0d8c8a8474a1d9eaa99db99
riley <rileywestonmiller@outlook.com>
2022-12-25 17:24:34 -0800

    first commit

---
+++ code_gen/code_gen.ts
@@ -0,0 +1,19 @@
+// const keywords: ([string, string]|string)[] = [
+//     'fn', ''
+// ];
+
+const keywords = 'if then else elif fn let null bool int float string';
+
+const char_names:[string, string][] = [
+    ['!', 'bang'],
+    ['=', 'equals'],
+    ['-', 'minus'],
+    ['+', 'plus'],
+    ['+', 'plus'],
+    ['*', 'asterisk'],
+    ['/', 'slash'],
+    ['&', 'slash'],
+];
+
+const symbols = '! = == != + - * /';
+
